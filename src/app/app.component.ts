db8df2968689c77afbe2b330a086025895f6181d
Kolomu <kevin-olomu@web.de>
2019-01-20 11:52:46 +0100

    test: get value of custom selector (getIsLoading)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Link } from './shared/link';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 import * as fromRoot from './app.reducers';
 import * as UI from './shared/ui.actions';
+
 
 @Component({
   selector: 'app-root',
@@ -17,11 +19,15 @@ export class AppComponent implements OnInit{
     { name: 'About', href: "#" }
   ];
 
+  isLoading$: Observable<boolean>;
+
   constructor(private store: Store<fromRoot.State>) { }
   
   /* Test the reducer */
   ngOnInit() {
     this.store.dispatch(new UI.StartLoading());
+
+    this.isLoading$ = this.store.select(fromRoot.getIsLoading);
 
     setTimeout( () => {
       this.store.dispatch(new UI.StopLoading());
